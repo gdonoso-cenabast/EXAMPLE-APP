a3236c7769de972db3a3c9aca9dc3fabd5927a51
Gonzalo Donoso <gdonoso@cenabast.cl>
2026-07-21 17:12:56 -0400

    10# COMMIT...

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ const titulo: HTMLHeadingElement = document.createElement('h1');
 
 titulo.style.fontWeight = 'bold';
 titulo.style.color = 'red';
-titulo.textContent = 'Example App';
+titulo.textContent = 'EJEMPLO DE APP CON JENKINS, SONARQUBE Y GITHUB';
 
 app.appendChild(titulo);
